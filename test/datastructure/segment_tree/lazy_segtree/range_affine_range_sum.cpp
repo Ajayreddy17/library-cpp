de224014d557b082acfa34f149a0ddbb61eb6172
Ajayreddy17 <96877307+Ajayreddy17@users.noreply.github.com>
2023-12-26 15:26:16 +0530

    bug fix

---
+++ test/datastructure/segment_tree/lazy_segtree/range_affine_range_sum.cpp
@@ -0,0 +1,31 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/range_affine_range_sum"
+
+#include <library/template.hpp>
+
+#include <library/mod/modint.hpp>
+#include <library/datastructure/segment_tree/lazy_segtree.hpp>
+#include <library/acted_monoid/am_sum_affine.hpp>
+
+using mint = modint998;
+
+int main(){
+    Int(n, q);
+
+    Vt(mint, a, n);
+    Lazy_SegTree<ActedMonoid_Sum_Affine<mint>> Lseg(a);
+    Loop(q){
+        Int(t);
+        if (t == 0){
+            Int(l, r);
+            mint b, c;
+            read(b, c);
+
+            Lseg.apply(l, r, {b, c});
+        }
+        else{
+            Int(l, r);
+            print(Lseg(l, r));
+        }
+    }
+    return 0;
+}

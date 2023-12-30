a3098d8e9d6bd1baf5e3894891d404a93b20d431
Ajayreddy17 <96877307+Ajayreddy17@users.noreply.github.com>
2023-12-30 17:04:51 +0530

    added tree ds

---
+++ library/graph/datastructure/static_tree_monoid.hpp
@@ -0,0 +1,135 @@
+#ifndef AJAY_STATIC_TREE_MONOID
+#define AJAY_STATIC_TREE_MONOID
+
+#include "library/datastructure/sparse_table/disjoint_sparse_table.hpp"
+#include "library/monoid/monoid_reverse.hpp"
+#include "library/graph/tree.hpp"
+
+namespace mitsuha{
+template <typename TREE, typename Monoid, bool edge>
+struct Static_Tree_Monoid {
+    using MX = Monoid;
+    using X = typename Monoid::value_type;
+    TREE &tree;
+    int N;
+    Disjoint_Sparse_Table<MX> seg;
+    Disjoint_Sparse_Table<Monoid_Reverse<MX>> seg_r;
+
+    Static_Tree_Monoid(TREE &tree) : tree(tree), N(tree.N) {
+        build([](int i) -> X { return MX::unit(); });
+    }
+    Static_Tree_Monoid(TREE &tree, vector<X> &dat) : tree(tree), N(tree.N) {
+        build([&](int i) -> X { return dat[i]; });
+    }
+    template <typename F>
+    Static_Tree_Monoid(TREE &tree, F f) : tree(tree), N(tree.N) {
+        build(f);
+    }
+
+    template <typename F>
+    void build(F f) {
+        if (!edge) {
+            auto f_v = [&](int i) -> X { return f(tree.V[i]); };
+            seg.build(N, f_v);
+            if constexpr (!MX::commute) seg_r.build(N, f_v);
+        } else {
+            auto f_e = [&](int i) -> X {
+                return (i == 0 ? MX::unit() : f(tree.v_to_e(tree.V[i])));
+            };
+            seg.build(N, f_e);
+            if constexpr (!MX::commute) seg_r.build(N, f_e);
+        }
+    }
+
+    X prod_path(int u, int v) {
+        auto pd = tree.get_path_decomposition(u, v, edge);
+        X val = MX::unit();
+        for (auto &&[a, b]: pd) { val = MX::op(val, get_prod(a, b)); }
+        return val;
+    }
+    // Last x on uv path where prod_path(u, x) satisfies check
+    // if not -1
+    template <class F>
+    int max_path(F check, int u, int v) {
+        if (edge) return max_path_edge(check, u, v);
+        if (!check(prod_path(u, u))) return -1;
+        auto pd = tree.get_path_decomposition(u, v, edge);
+        X val = MX::unit();
+        for (auto &&[a, b]: pd) {
+            X x = get_prod(a, b);
+            if (check(MX::op(val, x))) {
+                val = MX::op(val, x);
+                u = (tree.V[b]);
+                continue;
+            }
+            auto check_tmp = [&](X x) -> bool { return check(MX::op(val, x)); };
+            if (a <= b) {
+                // 下り
+                int i = seg.max_right(check_tmp, a);
+                return (i == a ? u : tree.V[i - 1]);
+            } else {
+                // 上り
+                int i = 0;
+                if constexpr (MX::commute) i = seg.min_left(check_tmp, a + 1);
+                if constexpr (!MX::commute) i = seg_r.min_left(check_tmp, a + 1);
+                if (i == a + 1) return u;
+                return (edge ? tree.parent[tree.V[i]] : tree.V[i]);
+            }
+        }
+        return v;
+    }
+
+    X prod_subtree(int u) {
+        int l = tree.LID[u], r = tree.RID[u];
+        return seg.prod(l + edge, r);
+    }
+
+    // [a,b] heavy path 形式
+    inline X get_prod(int a, int b) {
+        if constexpr (MX::commute)
+            return (a <= b ? seg.prod(a, b + 1) : seg.prod(b, a + 1));
+        return (a <= b ? seg.prod(a, b + 1) : seg_r.prod(b, a + 1));
+    }
+
+private:
+    template <class F>
+    int max_path_edge(F check, int u, int v) {
+        assert(edge);
+        if (!check(MX::unit())) return -1;
+        int lca = tree.lca(u, v);
+        auto pd = tree.get_path_decomposition(u, lca, edge);
+        X val = MX::unit();
+
+        for (auto &&[a, b]: pd) {
+            assert(a >= b);
+            X x = get_prod(a, b);
+            if (check(MX::op(val, x))) {
+                val = MX::op(val, x);
+                u = (tree.parent[tree.V[b]]);
+                continue;
+            }
+            auto check_tmp = [&](X x) -> bool { return check(MX::op(val, x)); };
+            int i = 0;
+            if constexpr (MX::commute) i = seg.min_left(check_tmp, a + 1);
+            if constexpr (!MX::commute) i = seg_r.min_left(check_tmp, a + 1);
+            if (i == a + 1) return u;
+            return tree.parent[tree.V[i]];
+        }
+        pd = tree.get_path_decomposition(lca, v, edge);
+        for (auto &&[a, b]: pd) {
+            assert(a <= b);
+            X x = seg.prod(a, b + 1);
+            if (check(MX::op(val, x))) {
+                val = MX::op(val, x);
+                u = (tree.V[b]);
+                continue;
+            }
+            auto check_tmp = [&](X x) -> bool { return check(MX::op(val, x)); };
+            auto i = seg.max_right(check_tmp, a);
+            return (i == a ? u : tree.V[i - 1]);
+        }
+        return v;
+    }
+};
+} // namespace mitsuha
+#endif // AJAY_STATIC_TREE_MONOID

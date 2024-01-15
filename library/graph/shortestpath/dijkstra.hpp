d33a509d2000c83924de0781248da43723d3c15d
Ajayreddy17 <96877307+Ajayreddy17@users.noreply.github.com>
2024-01-15 11:25:00 +0530

    added files

---
+++ library/graph/shortestpath/dijkstra.hpp
@@ -0,0 +1,89 @@
+#ifndef AJAY_DIJKSTRA
+#define AJAY_DIJKSTRA
+
+#include "library/graph/base.hpp"
+
+namespace mitsuha{
+template <typename T, typename GT>
+pair<vector<T>, vector<int>> dijkstra_dense(GT& G, int s) {
+    const int N = G.N;
+    vector<T> dist(N, numeric_limits::max() / 2);
+    vector<int> par(N, -1);
+    vector<bool> done(N);
+    dist[s] = 0;
+    while (1) {
+        int v = -1;
+        T mi = numeric_limits::max() / 2;
+        for(int i = 0; i < N; i++) {
+            if (!done[i] && chmin(mi, dist[i])) v = i;
+        }
+        if (v == -1) break;
+        done[v] = 1;
+        for (auto&& e: G[v]) {
+            if (chmin(dist[e.to], dist[v] + e.cost)) par[e.to] = v;
+        }
+    }
+    return {dist, par};
+}
+
+template <typename T, typename GT, bool DENSE = false>
+pair<vector<T>, vector<int>> dijkstra(GT& G, int v) {
+    if (DENSE) return dijkstra_dense<T>(G, v);
+    auto N = G.N;
+    vector<T> dist(N, numeric_limits::max() / 2);
+    vector<int> par(N, -1);
+    using P = pair<T, int>;
+
+    priority_queue<P, vector<P>, greater<P>> que;
+
+    dist[v] = 0;
+    que.emplace(0, v);
+    while (!que.empty()) {
+        auto [dv, v] = que.top();
+        que.pop();
+        if (dv > dist[v]) continue;
+        for (auto&& e: G[v]) {
+            if (chmin(dist[e.to], dist[e.frm] + e.cost)) {
+                par[e.to] = e.frm;
+                que.emplace(dist[e.to], e.to);
+            }
+        }
+    }
+    return {dist, par};
+}
+
+// Multi source. [dist, par, root]
+template <typename T, typename GT>
+tuple<vector<T>, vector<int>, vector<int>> dijkstra(GT& G, vector<int> vs) {
+    assert(G.is_prepared());
+    int N = G.N;
+    vector<T> dist(N, numeric_limits::max() / 2);
+    vector<int> par(N, -1);
+    vector<int> root(N, -1);
+
+    using P = pair<T, int>;
+
+    priority_queue<P, vector<P>, greater<P>> que;
+
+    for (auto&& v: vs) {
+        dist[v] = 0;
+        root[v] = v;
+        que.emplace(T(0), v);
+    }
+
+    while (!que.empty()) {
+        auto [dv, v] = que.top();
+        que.pop();
+        if (dv > dist[v]) continue;
+        for (auto&& e: G[v]) {
+            if (chmin(dist[e.to], dist[e.frm] + e.cost)) {
+                root[e.to] = root[e.frm];
+                par[e.to] = e.frm;
+                que.emplace(dist[e.to], e.to);
+            }
+        }
+    }
+    return {dist, par, root};
+}
+} // namespace mitsuha
+#endif // AJAY_DIJKSTRA
--- library/graph/shortestpath/grid_bfs.hpp
+++ library/graph/shortestpath/grid_bfs.hpp
@@ -4,38 +4,6 @@
 namespace mitsuha{
 // For example, walls = "#" or walls = {-1}.
 template <typename STRING>
-vector<vector<int>> grid_bfs(vector<STRING>& G, int sx, int sy, STRING walls, int dmax = 4) {
-    assert(dmax == 4 or dmax == 8);
-    int H = (int) G.size();
-    int W = (int) G[0].size();
-    auto isin = [&](int x, int y) -> bool {
-        if (x < 0 or H <= x) return false;
-        if (y < 0 or W <= y) return false;
-        return count(walls.begin(), walls.end(), G[x][y]) == 0;
-    };
-    int dx[] = {1, 0, -1, 0, 1, -1, -1, 1};
-    int dy[] = {0, 1, 0, -1, 1, 1, -1, -1};
-    vector<vector<int>> dist(H, vector<int>(W, numeric_limits<int>::max() / 2));
-    deque<pair<int, int>> que;
-    auto add = [&](int x, int y, int d) -> void {
-        if (!isin(x, y)) return;
-        if (d >= dist[x][y] ? false : (dist[x][y] = d, true)) que.emplace_back(x, y);
-    };
-    add(sx, sy, 0);
-
-    while (not que.empty()) {
-        auto [x, y] = que.front();
-        que.pop_front();
-        for(int d = 0; d < dmax; d ++) {
-            int nx = x + dx[d], ny = y + dy[d];
-            add(nx, ny, dist[x][y] + 1);
-        }
-    }
-    return dist;
-}
-
-// For example, walls = "#" or walls = {-1}.
-template <typename STRING>
 vector<vector<int>> grid_bfs(vector<STRING>& G, vector<pair<int, int>> &src, STRING walls, int dmax = 4) {
     assert(dmax == 4 or dmax == 8);
     int H = (int) G.size();
@@ -66,6 +34,13 @@ vector<vector<int>> grid_bfs(vector<STRING>& G, vector<pair<int, int>> &src, STR
         }
     }
     return dist;
+}
+
+// For example, walls = "#" or walls = {-1}.
+template <typename STRING>
+vector<vector<int>> grid_bfs(vector<STRING>& G, int sx, int sy, STRING walls, int dmax = 4) {
+    vector<pair<int, int>> src; src.emplace_back(sx, sy);
+    return grid_bfs(G, src, walls, dmax);
 }
 } // namespace mitsuha
 #endif // AJAY_GRID_BFS

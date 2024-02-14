784ebf91eb51874b07cab46c3952c063cbf42e5d
Ajayreddy17 <96877307+Ajayreddy17@users.noreply.github.com>
2024-02-14 19:48:03 +0530

    added files

--- library/util/binary_serch.hpp
+++ library/util/binary_serch.hpp
@@ -3,10 +3,10 @@
 
 namespace mitsuha{
 enum binary_search_tag { FirstFalse, FirstTrue, LastFalse, LastTrue };
-
-template <binary_search_tag tag, typename Arg, typename Fun,
-        std::enable_if_t<std::conjunction_v<std::is_invocable<Fun, Arg>, std::is_integral<Arg>>, std::nullptr_t> = nullptr>
-Arg binary_search_key_fisrt(Arg l, Arg r, const Fun &f) {
+enum auto_search_tag { True, False };
+ 
+template <binary_search_tag tag, typename Arg, typename Fun>
+Arg binary_search_fisrt(Arg l, Arg r, const Fun &f) {
     const bool invert = tag == binary_search_tag::FirstFalse;
     assert(invert ^ f(r));
     while (l < r) {
@@ -15,10 +15,9 @@ Arg binary_search_key_fisrt(Arg l, Arg r, const Fun &f) {
     }
     return l;
 }
-
-template <binary_search_tag tag, typename Arg, typename Fun,
-        std::enable_if_t<std::conjunction_v<std::is_invocable<Fun, Arg>, std::is_integral<Arg>>, std::nullptr_t> = nullptr>
-Arg binary_search_key_last(Arg l, Arg r, const Fun &f) {
+ 
+template <binary_search_tag tag, typename Arg, typename Fun>
+Arg binary_search_last(Arg l, Arg r, const Fun &f) {
     const bool invert = tag == binary_search_tag::LastFalse;
     assert(invert ^ f(l));
     while (l < r) {
@@ -27,19 +26,26 @@ Arg binary_search_key_last(Arg l, Arg r, const Fun &f) {
     }
     return l;
 }
-
-template <binary_search_tag tag, typename Arg, typename Fun>
-Arg binary_search_key(Arg l, Arg r, const Fun &f) {
+ 
+template <binary_search_tag tag, typename Arg, 
+        typename Fun, std::enable_if_t<std::conjunction_v<std::is_invocable<Fun, Arg>, std::is_integral<Arg>>, std::nullptr_t> = nullptr>
+Arg binary_search(Arg l, Arg r, const Fun &f) {
     if constexpr (tag == binary_search_tag::FirstTrue or tag == binary_search_tag::FirstFalse)
-        return binary_search_key_fisrt<tag>(l, r, f);
+        return binary_search_fisrt<tag>(l, r, f);
     else
-        return binary_search_key_last<tag>(l, r, f);
+        return binary_search_last<tag>(l, r, f);
 }
-
-template <binary_search_tag tag, typename Arg, typename Fun,
+ 
+template <auto_search_tag tag, typename Arg, typename Fun, 
         std::enable_if_t<std::conjunction_v<std::is_invocable<Fun, Arg>, std::is_integral<Arg>>, std::nullptr_t> = nullptr>
-std::invoke_result_t<Fun, Arg> binary_search_value(Arg l, Arg r, const Fun &f) {
-    return f(binary_search_key<tag>(l, r, f));
+Arg auto_binary_search(Arg l, Arg r, const Fun &f) {
+    assert(f(l) ^ f(r));
+    if constexpr (tag == auto_search_tag::True)
+        return f(l) ? binary_search<binary_search_tag::LastTrue>(l, r, f): 
+                binary_search<binary_search_tag::FirstTrue>(l, r, f);
+    else 
+        return f(l) ? binary_search<binary_search_tag::FirstFalse>(l, r, f):
+               binary_search<binary_search_tag::LastFalse>(l, r, f);
 }
 } // namespace mitsuha;
 #endif // AJAY_BINARY_SEARCH
